dbc11218514d330b95a574c89c84d09b37cd7b45
Damon Abdiel <damon.abdiel@gmail.com>
2021-01-28 05:16:22 +0100

    ajustei para renomear recursivamente os arquivos

--- renamewp.js
+++ renamewp.js
@@ -1,14 +1,22 @@
 var fs = require('fs');
+var path = require("path");
 
-const cont = './content/';
+const cont = '../content/';
 
-fs.readdir(cont, (err, files) => {
-	files.forEach(file => {
-		var newFile = file.substr(0, file.lastIndexOf(".")) + ".md";
-		console.log(`Rename ${file} to ${newFile}`);
 
-		fs.renameSync(`${cont}${file}`, `${cont}${newFile}`);
+function ThroughDirectory(Directory) {
+	fs.readdirSync(Directory).forEach(File => {
+		const file = path.join(Directory, File);
+		if (fs.statSync(file).isDirectory()) return ThroughDirectory(file);
+		else if (!file.includes('.DS_Store'))
+		{
+			var newFile = file.substr(0, file.lastIndexOf(".")) + ".md";
+  			console.log(`Rename ${file} to ${newFile}`);
+		  	fs.renameSync(`${file}`, `${newFile}`);
+		}
 	});
-});
+}
+
+ThroughDirectory(cont);
 
 console.log('Finalizado!');
